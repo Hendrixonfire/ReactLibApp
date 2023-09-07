45a126384e363b9cb3cfc411e2a323f1a51da955
Hendrixonfire <129618415+Hendrixonfire@users.noreply.github.com>
2023-09-07 20:08:42 +0300

    Add files via upload

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,3 +12,17 @@ export const decrement = (num) =>{
         payload: num
     }
 }
+
+export const SET_DARK_THEME ="SET_DARK_THEME"
+export const GOT_THEME = "https://www.nawpic.com/media/2020/game-of-thrones-nawpic-8.jpg"
+
+export const setTheme = (theme) =>(dispatch)=>{
+    dispatch({
+        type: 'SET_DARK_THEME',
+        payload: theme
+    })
+    dispatch({
+        type: 'GOT_THEME',
+        payload: theme
+    })
+}
